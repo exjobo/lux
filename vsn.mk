af9658007332fae2b1af90b4a83fee3e4f21d200
Håkan Mattsson <hm@tail-f.com>
2016-02-12 15:11:09 +0100

    Fix option handling crash

--- vsn.mk
+++ vsn.mk
@@ -3,4 +3,8 @@
 # See the file "LICENSE" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 
+<<<<<<< 171d66ceec501990d02f7c6134d1c9c3d853ddca
 LUX_VSN=1.12
+=======
+LUX_VSN=1.11.1
+>>>>>>> Fix option handling crash
